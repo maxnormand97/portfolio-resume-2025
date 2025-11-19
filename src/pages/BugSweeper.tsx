6f2ddf00124ec11410f81efaa1e7335f4c84b21c
Max Normand <maxnormand97@gmail.com>
2025-11-19 15:40:55 +0000

    functions to setup board, new interface and attributes for tile to set weather its a bug or not

--- src/pages/BugSweeper.tsx
+++ src/pages/BugSweeper.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import { useState } from 'react';
 import Tile from '../components/bug-sweeper/Tile'
 
-const BugSweeper: React.FC = () => {
   // TODO:
   // This will be like a mini minesweeper game but for bugs. The user will see something like the old minesweeper windows
   // game from the 2000s. There will be a bunch of tiles (we can make it configurable later) and within each tile there
@@ -24,24 +23,70 @@ const BugSweeper: React.FC = () => {
   // 2. It makes sense for the tile to contain a state, if its a bug or not, if it has been clicked or not (active - inactive) we
   // should add this as props to the Tile. We could also have a tile interface as well
 
-  function generateTiles(rows: number, cols: number) {
+interface BoardTile {
+  isBug: boolean;
+  isActive: boolean;
+}
+
+function createBoard(rows: number, cols: number, bugCount: number): BoardTile[][] {
+  // Create empty board that takes an array of board tiles set everything to default value
+  const board: BoardTile[][] = Array.from({ length: rows }, () =>
+    Array.from({ length: cols }, () => ({ isBug: false, isActive: false }))
+  );
+
+  // Randomly place bugs
+  let placed = 0;
+  while (placed < bugCount) {
+    const r = Math.floor(Math.random() * rows);
+    const c = Math.floor(Math.random() * cols);
+    if (!board[r][c].isBug) {
+      board[r][c].isBug = true;
+      placed++;
+    }
+  }
+  return board;
+}
+
+export default function BugSweeper() {
+  // TODO: we can configure this later so the user can set it
+  const rows = 4;
+  const cols = 4;
+  const bugCount = 5;
+
+  const [board, setBoard] = useState(() => createBoard(rows, cols, bugCount));
+
+  function handleTileClick(row: number, col: number) {
+    setBoard(prev =>
+      prev.map((rowArr, r) =>
+        rowArr.map((tile, c) =>
+          r === row && c === col ? { ...tile, isActive: true } : tile
+        )
+      )
+    );
+  }
+
+  // Create the game with tiles and tile props already set
+  function generateTiles(board: BoardTile[][]) {
     const tiles = [];
-    for (let r = 0; r < rows; r++) {
-      for (let c = 0; c < cols; c++) {
-        tiles.push(<Tile key={`${r}-${c}`} />);
+    for (let r = 0; r < board.length; r++) {
+      for (let c = 0; c < board[r].length; c++) {
+        const tile = board[r][c];
+        tiles.push(
+          <Tile
+            key={`${r}-${c}`}
+            isBug={tile.isBug}
+            isActive={tile.isActive}
+            onClick={() => handleTileClick(r, c)}
+          />
+        );
       }
     }
     return tiles;
   }
 
-  const rows = 8;
-  const cols = 8;
-
   return (
     <div style={{ width: cols * 36 }}>
-      {generateTiles(rows, cols)}
+      {generateTiles(board)}
     </div>
   );
 };
-
-export default BugSweeper;
